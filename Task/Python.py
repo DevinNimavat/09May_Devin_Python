dcf604dbb24052abc6a728a130c6480c48c67e79
DevinNimavat <devinnimavat888@gmail.com>
2024-05-21 20:35:09 -0700

    added

---
+++ Task/Python.py
@@ -0,0 +1,16 @@
+a=float(input("Enter a number"))
+b=float(input("Enter a nummber"))
+
+'''if num > 0:
+    print("number is positivev")
+
+elif num == 0:
+    print("thisis zero")
+
+else:
+    print("Numberis negative")'''
+
+print("Sum",a+b)
+print("Sub",a-b)
+print("Mul",a*b)
+print("Div",a/b)
